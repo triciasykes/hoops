c71772dea720e57384d0d2b31e962660e7fac959
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-04-24 20:06:04 -0700

    moved testlibrary to top of file

--- app/javascript/components/components/Champion.test.js
+++ app/javascript/components/components/Champion.test.js
@@ -1,6 +1,6 @@
+import {render} from "@testing-library/react"
 import React from "react"
 
-import {render} from "@testing-library/react"
 import { BrowserRouter } from "react-router-dom"
 import Champion from "./Champion"
 
